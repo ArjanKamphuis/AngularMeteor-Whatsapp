5a64c593936d6b0d1c7b3609e3bafdbb8816cdb1
Arjan <arjan_kamphuis@hotmail.com>
2016-03-18 19:11:12 +0100

    1 - Layout, coding style & structure

---
+++ client/scripts/routes.js
@@ -0,0 +1,19 @@
+angular.module('Whatsapp')
+    .config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('tab', {
+                url: '/tab',
+                abstract: true,
+                templateUrl: 'client/templates/tabs.html'
+            })
+            .state('tab.chats', {
+                url: '/chats',
+                views: {
+                    'tab-chats': {
+                        templateUrl: 'client/templates/chats.html',
+                        controller: 'ChatsCtrl as chats'
+                    }
+                }
+            });
+        $urlRouterProvider.otherwise('tab/chats');
+    });
