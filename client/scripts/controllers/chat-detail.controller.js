811823be3720c8667afe97ec03c0020f9ed29648
Arjan <arjan_kamphuis@hotmail.com>
2016-03-23 19:16:48 +0100

    8 - Send image messages

--- client/scripts/controllers/chat-detail.controller.js
+++ client/scripts/controllers/chat-detail.controller.js
@@ -1,5 +1,5 @@
 angular.module('Whatsapp')
-    .controller('ChatDetailCtrl', function($scope, $reactive, $stateParams, $timeout, $ionicScrollDelegate) {
+    .controller('ChatDetailCtrl', function($scope, $reactive, $stateParams, $timeout, $ionicScrollDelegate, $ionicPopup, $log) {
         $reactive(this).attach($scope);
         let chatId = $stateParams.chatId;
         let isIOS = ionic.Platform.isWebView() && ionic.Platform.isIOS();
@@ -12,6 +12,17 @@ angular.module('Whatsapp')
                 chatId: chatId
             });
             delete this.message;
+        };
+        this.sendPicture = () => {
+            MeteorCameraUI.getPicture({}, (err, data) => {
+                if (err && err.error === 'cancel') return;
+                if (err) return handleError(err);
+                Meteor.call('newMessage', {
+                    picture: data,
+                    type: 'picture',
+                    chatId: chatId
+                });
+            });
         };
         this.inputUp = () => {
             if (isIOS) {
@@ -44,4 +55,13 @@ angular.module('Whatsapp')
                 $ionicScrollDelegate.$getByHandle('chatScroll').scrollBottom(animate);
             }, 300);
         });
+        
+        function handleError(err) {
+            $log.error('send picture error', err);
+            $ionicPopup.alert({
+                title: err.reason || 'Send failed',
+                template: 'Please try again',
+                okType: 'button-positive button-clear'
+            });
+        }
     });
